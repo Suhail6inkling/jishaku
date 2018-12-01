f74bcd7d8a4e9338f674016dc3af808994e1d362
Starwort <tcphone93@gmail.com>
2018-12-01 07:28:00 +0000

    Check and fix extension loading

--- jishaku/cog.py
+++ jishaku/cog.py
@@ -708,21 +708,26 @@ class Jishaku:  # pylint: disable=too-many-public-methods
         await ctx.send("Logging out now..")
         await ctx.bot.logout()
     @commands.command()
+    @_check()
     async def run(self, ctx, *, argument: CodeblockConverter):
         await ctx.invoke(self.jsk_shell, argument = argument)
     @commands.command()
+    @_check()
     async def updatejsk(self, ctx, reload: bool = True):
         await ctx.invoke(self.jsk_shell, argument = Codeblock(None, 'python3 -m pip install -U git+https://github.com/Starwort/jishaku@master#egg=jishaku'))
-        if reload: await ctx.invoke(self.jsk_load, extensions = ['jishaku'])
+        if reload: await ctx.invoke(self.jsk_load, 'jishaku')
     @commands.command()
+    @_check()
     async def py(self, ctx, *, argument: CodeblockConverter):
         await ctx.invoke(self.jsk_python, argument = argument)
     @commands.command()
+    @_check()
     async def unload(self, ctx: commands.Context, *extensions):
-        await ctx.invoke(self.jsk_unload, extensions = extensions)
+        await ctx.invoke(self.jsk_unload, *extensions)
     @commands.command(aliases=["reload"])
+    @_check()
     async def load(self, ctx: commands.Context, *extensions):
-        await ctx.invoke(self.jsk_load, extensions = extensions)
+        await ctx.invoke(self.jsk_load, *extensions)
 def setup(bot: commands.Bot):
     """
     Adds the Jishaku cog to the bot.
