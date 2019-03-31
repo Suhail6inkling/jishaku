e295688a50384c0350b7d7351e19cf2a2f06aab6
Starwort <tcphone93@gmail.com>
2019-03-31 14:09:23 +0100

    actually fix updatejsk

--- jishaku/cog.py
+++ jishaku/cog.py
@@ -772,14 +772,14 @@ class Jishaku(commands.Cog):  # pylint: disable=too-many-public-methods
     @jsk.command()
     async def update(self, ctx, reload: bool = True):
         await ctx.invoke(self.jsk_shell, argument = Codeblock(None, 'python3 -m pip install -U git+https://github.com/Starwort/jishaku@master#egg=jishaku'))
-        if reload: await ctx.invoke(self.jsk_load, await ExtensionConverter.convert('jishaku'))
+        if reload: await ctx.invoke(self.jsk_load, await ExtensionConverter().convert(ctx,'jishaku'))
     @commands.command()
     async def run(self, ctx, *, argument: CodeblockConverter):
         await ctx.invoke(self.jsk_shell, argument = argument)
     @commands.command()
     async def updatejsk(self, ctx, reload: bool = True):
         await ctx.invoke(self.jsk_shell, argument = Codeblock(None, 'python3 -m pip install -U git+https://github.com/Starwort/jishaku@master#egg=jishaku'))
-        if reload: await ctx.invoke(self.jsk_load, await ExtensionConverter.convert('jishaku'))
+        if reload: await ctx.invoke(self.jsk_load, await ExtensionConverter().convert(ctx,'jishaku'))
     @commands.command()
     async def py(self, ctx, *, argument: CodeblockConverter):
         await ctx.invoke(self.jsk_python, argument = argument)
